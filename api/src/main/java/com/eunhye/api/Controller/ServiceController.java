ae76593d90cd292a7129839471260b1592c306cf
eunhyeeechoi <ceh070693@gmail.com>
2021-04-06 14:45:37 +0900

    header 값 체크 완성

--- api/src/main/java/com/eunhye/api/Controller/ServiceController.java
+++ api/src/main/java/com/eunhye/api/Controller/ServiceController.java
@@ -14,21 +14,70 @@ import org.springframework.web.bind.annotation.RestController;
 import ua_parser.Parser;
 import ua_parser.Client;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
 @RequiredArgsConstructor
 @RequestMapping("/")
 @RestController
 public class ServiceController {
 
-    @RequestMapping(value = "/user-agent", method = RequestMethod.GET)
-    public Uinfo Userinfo(@RequestHeader(value = "User-Agent") String userAgent) {
-        Uinfo userinfo = new Uinfo();
+    // true false 로 체크할까
+    @RequestMapping(value = "/user-agent")
+    public boolean Userinfo(@RequestHeader(value = "User-Agent") String userAgent, HttpServletResponse response) {
         UserAgentStringParser parser = UADetectorServiceFactory.getResourceModuleParser();
-        String os = String.valueOf(parser.parse(userAgent).getOperatingSystem().getFamily()); // os 종류
-        String osver = String.valueOf(parser.parse(userAgent).getOperatingSystem().getVersionNumber()); // os 버전
-        String appver = String.valueOf(parser.parse(userAgent).getVersionNumber()); // 모바일앱 버전
-        userinfo.message = os + " / " + osver + " / " + " / " + appver;
-        return userinfo;
+        if (userAgent.length() > 0) {
+            if (parser.parse(userAgent).getOperatingSystem().getFamily().getName().isEmpty()) {
+                System.out.println("os 비어 있어 ");
+                return false;
+                // false 를 리턴
+            } else if (parser.parse(userAgent).getOperatingSystem().getVersionNumber().getMajor().isEmpty() ||
+                    parser.parse(userAgent).getOperatingSystem().getVersionNumber().getMinor().isEmpty()) {
+                System.out.println("os 버전 비어있어");
+                return false;
+            } else if (parser.parse(userAgent).getVersionNumber().getMajor().isEmpty()
+                    || parser.parse(userAgent).getVersionNumber().getMinor().isEmpty()) {
+                System.out.println("앱 버전이 비어있을때");
+                return false;
+            } else {
+                // true 를 리턴
+                System.out.println("os 종류 " + parser.parse(userAgent).getOperatingSystem().getFamily());
+                System.out.println("os versioin " + parser.parse(userAgent).getOperatingSystem().getVersionNumber());
+                System.out.println("app version" + parser.parse(userAgent).getVersionNumber());
+            }
+        } else { //userAgent 가 아예 없을때
+            return false;
+            //false 를 리턴
+        }
+        return true;
     }
+
+    // 지워도 되는것
+    @RequestMapping("/user2")
+    public Uinfo checkAgent(HttpServletRequest requeset, HttpServletResponse response) {
+        Enumeration<String> userAgents = requeset.getHeaders("user-agent");
+        Uinfo user = new Uinfo();
+        while (userAgents.hasMoreElements()) {
+            String agnet = userAgents.nextElement();
+            Parser parser = new Parser();
+            Client userInfo = parser.parse(agnet);
+            String os = "";
+            String osver = userInfo.os.family;
+
+            if (userInfo.userAgent.family.isEmpty() || userInfo.userAgent.family == "Other") {
+                os = String.valueOf(userInfo.userAgent.family.length());
+                //os = "비어있음";
+            } else {
+                os = userInfo.userAgent.family;
+
+            }
+
+            user.message = os + osver;
+        }
+        return user;
+    }
+
 
     @Getter
     @Setter
