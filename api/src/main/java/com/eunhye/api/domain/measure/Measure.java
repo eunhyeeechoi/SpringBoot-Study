8453c643f57c1adfb0061fa4ddc7cc72e5ff4f07
eunhyeeechoi <ceh070693@gmail.com>
2021-04-08 22:17:30 +0900

    소스정리 및 테스트

--- api/src/main/java/com/eunhye/api/domain/measure/Measure.java
+++ api/src/main/java/com/eunhye/api/domain/measure/Measure.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Table(name = "measure1")
+@Table(name = "measure")
 public class Measure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
